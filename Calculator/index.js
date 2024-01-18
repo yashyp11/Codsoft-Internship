00d700238d9a3fe5c605273df8ed35ac75836c02
yashyp11 <sanandiayyash1166@gmail.com>
2024-01-18 14:59:07 +0530

    first commit

---
+++ Calculator/index.js
@@ -0,0 +1,21 @@
+let string = "";
+let buttons = document.querySelectorAll('.btnn');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else {
+
+            console.log(e.target);
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
+    })
+})
